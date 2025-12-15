67eb4a46785afa9c58f1dd0a6b1450846475fd0b
Ori Hanuna <orihanuna10@gmail.com>
2025-12-15 21:16:24 +0200

    commit - Q60 Completed

---
+++ HannukahAssignments.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignments
+{
+    internal class HannukahAssignments
+    {
+        static void Main(string[] args)
+        {
+            /*Question 60 - Coin Toss
+            int countL=0,countS=0,i=1;
+            bool win= false;
+            while (!win)
+            {
+                Console.Write($"Round #{i}, Lee's turn(E/P): ");
+                if (Char.ToLower(char.Parse(Console.ReadLine())) == 'e') { countL++; }
+                if (countL == 3) { win = true; }
+                if (!win)
+                {
+                    Console.Write($"Round #{i}, Shir's turn(E/P): ");
+                    if (Char.ToLower(char.Parse(Console.ReadLine())) == 'e') { countS++; }
+                    if (countS == 3) { win = true; }
+                    i++;
+                }
+            }
+            Console.WriteLine($"{(countL==3?"Lee":"Shir")} will start the Checkers match.");
+            Console.ReadKey();*/
+
+        }
+    }
+}
